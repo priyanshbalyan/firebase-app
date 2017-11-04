3f5db25b6ed4032e5046d9545a2caa128675a0df
Priyansh Balyan <priyanshbalyan@gmail.com>
2017-11-04 17:57:26 +0530

    Admin dashboard added

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const firebase = require('firebase');
-const fs = require('fs');
 const bcrypt = require('bcrypt');
+
+let bodyParser = require('body-parser');
+let multer = require('multer');
+let upload = multer();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
@@ -15,27 +21,41 @@ let config = {
     projectId: "server-eb0a3",
     storageBucket: "server-eb0a3.appspot.com",
     messagingSenderId: "833102713061"
-  };
+};
 
 firebase.initializeApp(config);
 let database = firebase.database();
-
-// app.listen(3000, function(){	
-// 	console.log('Listening on port 3000');
-// });
-
-app.use(express.static('html'));
-
-app.get('/', (req, res)=>{
-	res.end('heelo');
-});
 
 http.listen(process.env.PORT || 3000, ()=>{
 	console.log('listening on : '+(process.env.PORT||3000));
 });
 
+app.get('/', (req, res)=>{
+	res.end('heelo');
+});
 
-io.on('connection', (socket)=>{
+// app.get('/dashboard.html', upload.array(), (req, res, next)=>{
+// 	database.ref('database/users/'+req.headers.username).once('value').then(snapshot=>{
+// 		if(!snapshot || !bcrypt.compareSync(req.headers.password, snapshot.val().password))	 
+// 			return res.sendFile(__dirname+'/html/index.html');
+
+// 		if(snapshot.val().level == 'exec'){
+// 			console.log('exec trigger');
+// 			return res.sendFile(__dirname+'/html/admindashboard.html');
+// 		}
+// 		res.sendFile(__dirname+'/html/dashboard.html');
+// 	}).catch(err=>{
+// 		res.sendFile(__dirname+'/html/index.html');
+// 		console.log(err.message);
+// 	});
+	
+// });
+
+
+
+app.use(express.static('html'));
+
+io.on('connection', socket=>{
 	console.log('a user connected');
 	socket.on('disconnect', ()=>{
 		console.log('user disconnected');
@@ -44,7 +64,7 @@ io.on('connection', (socket)=>{
 	socket.on('signup', data=>{
 		//console.log(data);
 		if(data.password != data.confirm_password)
-			io.emit('alert', "Error. Psswords don't match");
+			io.emit('alert', "Error. Passwords don't match");
 		else{
 			database.ref('database/users/'+data.username).once('value').then(snapshot=>{
 				console.log(snapshot.val());
@@ -56,7 +76,8 @@ io.on('connection', (socket)=>{
 						fullname:data.fullname,
 						phone:data.phone,
 						password:bcrypt.hashSync(data.password, 10),
-						level:'normal'
+						level:'normal',
+						acc_created:Date.now()
 					});
 					io.emit('alert', "Account created");
 				}
@@ -71,10 +92,11 @@ io.on('connection', (socket)=>{
 		database.ref('database/users/'+data.username).once('value').then(snapshot=>{
 			if(!snapshot || !bcrypt.compareSync(data.password, snapshot.val().password))	 
 				return io.emit('alert', {message:'Incorrect username or password',username:"", success:false});
-			io.emit('alert', {message:'Signed in as '+data.username, username:data.username, success:true});
+
+			io.emit('alert', {message:'Signed in as '+data.username, username:data.username, success:true, privilege:snapshot.val().level});
 		}).catch(err=>{
 			io.emit('alert', {message:'An error occured while signing in, please try later.',username:"", success:false});
-			console.log(err.message);
+			console.log(err);
 		});
 	});
 
@@ -82,20 +104,40 @@ io.on('connection', (socket)=>{
 		console.log('Dashboard load', username);
 		database.ref('database/users/'+username).once('value').then(snapshot=>{
 			data = snapshot.val();
-			delete data.password;
 			io.emit('load', data);
 			//console.log(snapshot.val());
 		}).catch(err=>console.log("Error loading data. "+err.message));
 
 	});
 
+	socket.on('admindashboard', admin=>{
+
+		database.ref('database/users/'+admin).once('value').then(snapshot=>{
+			data = snapshot.val();
+			io.emit('load', snapshot.val());
+			//console.log(snapshot.val());
+		}).catch(err=>console.log("Error loading data. "+err.message));
+
+		database.ref('database/wallet/').once('value').then(snapshot=>{
+			//console.log(snapshot.val());
+			io.emit('walletload', snapshot.val());
+		}).catch(err=>console.log(err.message));
+
+		database.ref('database/users/').once('value').then(snapshot=>{
+			io.emit('userload', snapshot.val());
+		}).catch(err=>console.log(err.message));
+	});
+
 	socket.on('investment', data=>{
+
 		database.ref('database/wallet/'+data.timestamp).set({
 			amount:data.amount,
 			timestamp:data.timestamp,
-			created_by:data.username
+			created_by:data.username,
+			status:'pending'
 		});
 
+		console.log('invested : ', data);
 		io.emit('update', data);
 		io.emit('alert', 'Investment made.');
 	});
